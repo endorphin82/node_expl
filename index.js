b79baf8b265e9db1b0074c5582994e08d057961d
endorphin82 <endorphin82@gmail.com>
2017-11-03 10:46:33 +0300

    http://nodebeginner.ru

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ console.log("Hello from JavaScript!");
 console.log(moment().startOf('day').fromNow());
 var name = "Andrey", time = "today";
 console.log(`Hello ${name}, how are you ${time}?`);
+// server.js
+var server = require("./server");
+var router = require("./router");
+
+server.start(router.route);
